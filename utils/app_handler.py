430b796cbddb04c7cd72cb0d7c4c599d9a7f52e9
luluw <shashinmhrzn@gmail.com>
2024-02-20 17:06:42 +0545

    [FIX/ADD] Weather API calls/Renaming Feature

--- utils/app_handler.py
+++ utils/app_handler.py
@@ -1,41 +1,50 @@
 import os 
-import os
 import platform
 
-# Create Folder
-def create_folder(folder_name):
-    # Get the absolute path of the folder
+# Get the current working directory
+def get_absolute_path(folder_name):
     path = os.path.abspath(folder_name)
-
-    # Check the operating system
     if platform.system() == 'Windows':
         path = os.path.join('C:', os.sep, path)
     else:
         path = os.path.join('/', path)
+    return path
 
-    # Check if the folder already exists
+# Create Folder
+def create_folder(folder_name):
+    # Get the absolute path of the folder
+    path = get_absolute_path(folder_name)
+
     if os.path.exists(path):
         print(f'The folder "{folder_name}" already exists at {path}')
         print('Please choose another name.')
     else:
-        # Create the folder
         os.makedirs(path, exist_ok=True)  # Use exist_ok=True for recursive creation
         print(f'Folder "{folder_name}" has been created successfully at {path}')
 
 # Delete Folder
 def delete_folder(folder_name):
-    path = os.path.abspath(folder_name)
-
-    if platform.system() == 'Windows':
-        path = os.path.join('C:', os.sep, path)
-    else:
-        path = os.path.join('/', path)
+    path = get_absolute_path(folder_name)
 
     if os.path.exists(path):
         os.rmdir(path)
         print(f'Folder "{folder_name}" deleted successfully.')
     else:
         print(f'The folder named "{folder_name}" does not exist.')
+
+# Rename Folder
+def rename_folder(old_folder_name, new_folder_name):
+    old_path = get_absolute_path(old_folder_name)
+    new_path = get_absolute_path(new_folder_name)
+    
+    if os.path.exists(old_path):
+        if os.path.exists(new_path):
+            print(f'A folder or file with the name "{new_folder_name}" already exists.')
+        else:
+            os.rename(old_path, new_path)
+            print(f'Folder "{old_folder_name}" renamed to "{new_folder_name}" successfully.')
+    else:
+        print(f'The folder named "{old_folder_name}" does not exist.')
 
 
 # Search Applications/Files/Folders

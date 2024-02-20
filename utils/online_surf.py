3f6bf2e202c0922b1b8d17195c0205ba0a6da093
luluw <shashinmhrzn@gmail.com>
2024-02-20 22:21:11 +0545

    [FIX] Launch Google and Redirect Search

--- utils/online_surf.py
+++ utils/online_surf.py
@@ -1,22 +1,38 @@
 from googlesearch import search
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import time
 import pywhatkit as kit
-import os 
+import webbrowser
+# import os 
 
 # Replace Path_to_ChromeDriver Executable file in .env
-path_to_chromedriver = os.getenv('chromedriver')
+# path_to_chromedriver = os.getenv('chromedriver')
 
 
+# Google Search
+# NOTE: Chrome must be installed in default path 
 def google_search(query, num_results=5):
     try:
-        search_results = list(search(query, num_results=num_results))
-        return search_results
+        chrome_path = "C:/Program Files/Google/Chrome/Application/chrome.exe %s"
+        # print(chrome_path)
+
+        if chrome_path:
+            search_results = list(search(query, num_results=num_results))
+            if search_results:
+                first_result = search_results[0]
+                # print(first_result)
+
+                # Open chrome and open first search_results
+                webbrowser.get(chrome_path).open(first_result)
+                print("Opened the first search result in Google Chrome.")
+                return search_results
+            else:
+                print("No search results found.")
+        else:
+            print("Chrome path not found in app_paths.json. Cannot open Chrome.")
     except Exception as e:
         print("An error occurred while performing the Google search:", e)
         return []
 
+# YouTube Search
 def youtube_search(query):
     try:
         video_url = kit.playonyt(query)

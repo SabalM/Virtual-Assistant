a77843ef063db8a20ff23b9ea7f3f4e803e2e03a
luluw <shashinmhrzn@gmail.com>
2024-02-21 16:28:24 +0545

    :rocket: Speech Launch Applications

--- main.py
+++ main.py
@@ -1,186 +1,214 @@
-import random
+import torch
 import os
-import time
-import requests
 import json
-from core.engine import *
-from core.intents import *
-# from core.manage_intents import *
+import random
 
-# Load Secret Keys
+# import Speech Engine
+from engine import *
+
+# import Intent
+from neuralnet.model import IntentModelClassifier
+from neuralnet.nltk_utils import bag_of_words, tokenize
+
+# import task automation functions
+from utils.app_handler import *
+from utils.weather import *
+from utils.online_surf import *
+from utils.device_control import *
+from utils.application import *
+from utils.volume_control import ActionHandler
+
+
+# Instantiate ActionHandler for volume control
+action_handler = ActionHandler()
+
+# Load API keys from .env
 from dotenv import load_dotenv
 load_dotenv()
+
+# Loading Application Paths JSON file 
+with open('utils/app_paths.json', 'r') as f:
+    application_paths = json.load(f)
+
+# Setting device agnostic code
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print(f"Loaded model on: {device}")
+
+with open('intents.json', 'r') as json_data:
+    intents = json.load(json_data)
+
+""" Load Intent_Classifier Trained Model """
+MODEL_SAVE_PATH = "model/intent.pth"
+model_info = torch.load(MODEL_SAVE_PATH)
+
+input_size = model_info["input_size"]
+hidden_size = model_info["hidden_size"]
+output_size = model_info["output_size"]
+all_words = model_info["all_words"]
+tags = model_info["tags"]
+model_state = model_info["model_state"]
+
+# Instantiate IntentModelClassifier
+model = IntentModelClassifier(
+    input_size,
+    hidden_size,
+    output_size).to(device)
+
+model.load_state_dict(model_state)
+model.eval()
+
 
 # ANSI escape codes for colors
 GREEN = "\033[92m"
 RED = "\033[91m"
+BLUE = "\033[96m"
 RESET = "\033[0m"
 
-# Matched Intent Response
-def response(matched_intent,
-             city=None,
-             description=None,
-             temperature=None):
 
-    # Functional Intent Response
-    if matched_intent == 'weather':
-        weather_responses = [response['responses']
-                             for response in intents if response['tag'] == 'weather']
-        if weather_responses:
-            response = random.choice(weather_responses[0])
-            print(GREEN + response.format(city=city,
-                                          description=description,
-                                          temperature=temperature) + RESET)
-        else:
-            print(RED + "Error: Unable to find weather data. Check your internet connection." + RESET)
+bot_name = "ByteBot"
+recognizer, mic, stream = initialize_model()
+print("-"*50)
+print(f"{GREEN}Welcome to ByteBot!{RESET}\n{BLUE}To exit, simply say {RED}'quit'{BLUE}.\n{GREEN}Speech Recognition{BLUE} is now initialized.{RESET}")
+print("-"*50)
 
-    # Default response
+while True:
+    # sentence = input(f"{GREEN}You{RESET}: ")
+    sentence = speech_recognize(recognizer, stream)
+    print(f"{GREEN}You{RESET}: {sentence}")
+    if sentence == "quit":
+        break
+
+    sentence = tokenize(sentence)
+    X = bag_of_words(sentence, all_words)
+    # print(X.shape[0])
+    X = X.reshape(1, X.shape[0])
+    X = torch.from_numpy(X).to(device)
+
+    output = model(X)
+    _, predicted = torch.max(output, dim=1)
+    tag = tags[predicted.item()]
+    # print(f"Bot: {bot_name}, {tag}")
+
+    # Assigns all proabability in range [0, 1]
+    probs = torch.softmax(output, dim=1)
+    prob = probs[0, predicted.item()]
+
+    if prob.item() > 0.75:
+        for intent in intents['intents']:
+            if tag == intent['tag']:
+                response = random.choice(intent['responses'])
+
+                # Create Folder
+                if tag == "Create":
+                    # print(user_input)
+                    print(f"{RED}{bot_name}{RESET}: {response}")
+                    folder_name = speech_recognize(recognizer, stream)
+                    print(f"{BLUE}Folder name: {RESET}{folder_name}")
+                    create_folder(folder_name)
+
+                # Delete Folder
+                elif tag == "Delete":
+                    print(f"{RED}{bot_name}{RESET}: {response}")
+                    folder_name = speech_recognize(recognizer, stream)
+                    print(f"{BLUE}Folder name: {RESET}{folder_name}")
+                    delete_folder(folder_name)
+                
+                # Rename Folder
+                elif tag == "Rename":
+                    print(f"{RED}{bot_name}{RESET}: {response}")
+                    old_folder_name = speech_recognize(recognizer, stream)
+                    print(f"{BLUE}Old Folder name: {RESET}{old_folder_name}")
+                    print(f"{RED}{bot_name}{RESET}: Give new name for folder `{old_folder_name}`")
+                    new_folder_name = speech_recognize(recognizer, stream)
+                    print(f"{BLUE}New Folder name: {RESET}{new_folder_name}")
+                    rename_folder(old_folder_name, new_folder_name)
+
+                # Weather Forecast
+                elif tag == "weather":
+                    print(f"{RED}{bot_name}{RESET}: {response}")
+                    city =  speech_recognize(recognizer, stream)
+                    print(f"{BLUE}Location: {RESET}{city}")
+                    city, description, temperature = weather_forecast(city)
+                    print(
+                        f"{RED}{bot_name}{RESET}: In {city}, the temperature is {temperature} degrees Celsius. The weather condition is {description}.")
+
+                # Google Search
+                elif tag == "google":
+                    print(f"{RED}{bot_name}{RESET}: {response}")
+                    query = speech_recognize(recognizer, stream)
+                    print(f"{BLUE}Search Query: {RESET}{query}")
+                    search_results = google_search(query)
+                    if search_results:
+                        print(
+                            f"{RED}{bot_name}{RESET}: Here are the search results ->")
+                        for result in search_results:
+                            print(result)
+                        print(
+                            f"{BLUE}Opening{RESET}: {search_results[0]}")
+                    else:
+                        print(
+                            f"{RED}{bot_name}{RESET}: No search results found for '{query}'.")
+
+                # YouTube Search
+                elif tag == "youtube":
+                    print(f"{RED}{bot_name}{RESET}: {response}")
+                    query = speech_recognize(recognizer, stream)
+                    print(f"{BLUE}Search Query: {RESET}{query}")
+                    video_url = youtube_search(query)
+                    print(f"{RED}{bot_name}{RESET}: Opening YouTube -> {video_url}")
+
+                # Device control
+                elif tag == "Sleep" or tag == "Shutdown" or tag == "LogOff":
+                    print(f"{RED}{bot_name}{RESET}: Enter device control mode once again to verify\n1. sleep\n2. shutdown\n3. logoff\n")
+                    choice = speech_recognize(recognizer, stream).lower()
+                    cmd = system_control(choice)
+                    print(f"{BLUE}Running script: {cmd}{RESET}")
+                    
+                    # TODO: Exception for timer loop
+                    for timer in range(5, 0, -1):  # Countdown from 5 to 1
+                        print(f"{RED}{bot_name}{RESET}: {response} in {timer} sec{RESET}", end="\r")
+                        time.sleep(1)  # 1-second delay between countdown messages
+                    print(f"{RED}{bot_name}{RESET}: {response} now{RESET}")
+
+                # TODO: Email 
+                # Sending E_mail
+                elif tag == "E_mail":
+                    pass
+                  
+                # Volume Control
+                elif tag == "Volume_Up":
+                    print(f"{RED}{bot_name}{RESET}: {response}")
+                    action_handler.volume_increase()
+
+                elif tag == "Volume_Down":
+                    print(f"{RED}{bot_name}{RESET}: {response}")
+                    action_handler.volume_decrease()
+
+                # Open Application 
+                elif tag == "Open_Application":
+                    print(f"{RED}{bot_name}{RESET}: {response}")
+                    input_sentence = ' '.join(sentence)
+                    # print(sentence, input_sentence)
+                    application_name = extract_application_name(input_sentence)
+                    if application_name:
+                        open_application(application_name.lower(), application_paths)
+                        print(f"Opened {application_name}")
+                    else:
+                        print("No application name found in the input sentence.")
+
+
+
+
+                # TODO: Storing messages in txt or docx file
+                # TODO: Reminder, Alarm, Events, To-do list
+                # TODO: ChatGPT
+
+                # TODO: Final Speech Transcription Integration
+
+                else:
+                    print(f"{RED}{bot_name}{RESET}: {response}")
+                break
+
+                
     else:
-        response = get_response(matched_intent, intents)
-        print(GREEN + response + RESET)
-
-
-"""Creation and Deletion of Directories"""
-
-
-def create_folder():
-    folder_name = speech_recognize(recognizer, stream)
-    path = os.path.abspath(folder_name)
-
-    if os.path.exists(path):
-        print(
-            GREEN + f'The folder "{folder_name}" already exists at {path}' + RESET)
-        print(RED + 'Please choose another name.' + RESET)
-    else:
-        os.mkdir(path)
-        print(
-            GREEN + f'Folder "{folder_name}" has been created successfully at {path}' + RESET)
-
-
-def delete_folder():
-    folder_name = speech_recognize(recognizer, stream)
-    path = os.path.abspath(folder_name)
-
-    if os.path.exists(path):
-        os.rmdir(path)
-        print(RED + f'Folder "{folder_name}" deleted successfully.' + RESET)
-    else:
-        print(
-            RED + f'The folder named "{folder_name}" does not exist.' + RESET)
-
-
-""" Weather Forecast """
-
-
-def weather_forecast():
-    responses = [
-        f"Sure! What city would you like the weather forecast for?",
-        f"I'd be happy to check the weather. Could you please tell me the city?",
-        f"Great! To provide the weather forecast, I just need to know the city name. What city are you interested in?"
-    ]
-
-    print(GREEN +  random.choice(responses) + RESET)
-
-    city = speech_recognize(recognizer, stream)
-
-    # If the recognized city is blank, set default to "Kathmandu"
-    city = city if city.strip() else "Kathmandu"
-
-    base_url = 'http://api.openweathermap.org/data/2.5/weather?'
-    api_key = os.getenv("WEATHER_API_KEY")
-    url = f"{base_url}q={city}&appid={api_key}"
-    response = requests.get(url)
-
-    try:
-        response.raise_for_status()
-    except requests.exceptions.HTTPError as err:
-        print(f"HTTP Error: {err}")
-        return f"Error: Could not retrieve weather data for {city}. Please try again later."
-
-    if response.status_code == 200:
-        # Save JSON response to a file
-        file_path = 'data_archive/weather_data.json'
-        with open(file_path, 'w', encoding='utf-8') as json_file:
-            json.dump(response.json(), json_file, ensure_ascii=False, indent=4)
-
-        # Load JSON file
-        with open(file_path, 'r', encoding='utf-8') as json_file:
-            data = json.load(json_file)
-
-        description = data.get("weather", [{}])[0].get("description")
-
-        # Extracting temperature (Kelvin to Celsius)
-        temperature = data.get("main", {}).get("temp")
-        if temperature is not None:
-            temperature -= 273.15
-            temperature = round(temperature, 2)
-        else:
-            temperature = "N/A"
-
-        return city, description, temperature
-    else:
-        print(
-            f"Error: {response.status_code}. Could not retrieve weather data for {city}.")
-        return f"Error: Could not retrieve weather data for {city}. Please try again later."
-
-
-
-if __name__ == "__main__":
-    # Intent Path
-    intents = load_intents('intents.json')
-
-    # Initialize model
-    recognizer, mic, stream = initialize_model()
-    print("Model Loaded Successfully...\n")
-    print(GREEN + "Hello! Ask me anything or say 'goodbye' to exit." + RESET)
-
-    while True:
-        try:
-            user_input = speech_recognize(recognizer, stream)
-            print(user_input)
-
-            matched_intent = None
-            for item in intents:
-                if match_intent(user_input, item):
-                    matched_intent = item['tag']
-                    break
-
-            if matched_intent == 'Create':
-                response(matched_intent)
-                create_folder()
-
-            elif matched_intent == 'Delete':
-                response(matched_intent)
-                delete_folder()
-
-            elif matched_intent == 'weather':
-                try:
-                    city, description, temperature = weather_forecast()
-                    # If the speech_recognizer is not able to parse city name re-run the recognizer instance
-                    while not city:
-                        print("I'm sorry, I couldn't understand the city name. Could you please rephrase it?")
-                        city = speech_recognize(recognizer, stream)
-                        if not city:
-                            print("I still couldn't understand the city name. Please try again.")
-
-                    response(matched_intent, city, description, temperature)
-                except ValueError as e:
-                    print(f"Error: {e}")
-                    print("Weather data retrieval failed.")
-                except Exception as e:
-                    print(f"Unexpected error: {e}")
-                    print("An unexpected error occurred during weather data retrieval.")
-                    time.sleep(1)  # Sshort sleep to handle errors and prevent continuous looping
-                    break  # Stop the loop if an exception occurs
-            
-            else:
-                response(matched_intent)
-
-            # Terminate session
-            for intent in intents:
-                if intent['tag'] == 'goodbye' and match_intent(user_input, intent):
-                    exit()
-
-        except Exception as e:
-            print(f"Error in speech recognition: {e}")
-            time.sleep(1)
+        print(f'{RED}{bot_name}{RESET}: I donot understand...')

65db6ca339701d5665bb8f6a6bcd1de3cc160d59
luluw <shashinmhrzn@gmail.com>
2024-03-10 10:54:04 +0545

    :pencil: Changes App_Paths & Email JSON

--- utils/e_mail.py
+++ utils/e_mail.py
@@ -1,13 +1,10 @@
 import os
 import smtplib
 from email.message import EmailMessage
+import json
+from dotenv import load_dotenv
 
-dict = {
-    'saugat':'saugat556513@gmail.com',
-    'neema':'neemalimbu04@gmail.com',
-    'searching': 'shashinmhrzn@gmail.com',
-    'server': 'maharjansabal@gmail.com'     # since speech model is not accuracte placed a close sounding key name
-}
+load_dotenv()
 
 # ANSI escape codes for colors
 GREEN = "\033[92m"
@@ -15,32 +12,33 @@ RED = "\033[91m"
 BLUE = "\033[96m"
 RESET = "\033[0m"
 
-
 bot_name = "ByteBot"
 
 def send_mail(receiver, subject, message):
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
-    server.login('saugatthapa344@gmail.com','yqsr zlkc vutg oouu')
-    email=EmailMessage()
-    email['from']="saugatthapa344@gmail.com"
-    email['to']=receiver
-    email['subject']=subject
+    email_address = os.getenv('EMAIL_ADDRESS')
+    email_secret_key = os.getenv('EMAIL_KEY')
+    server.login(email_address, email_secret_key)
+    email = EmailMessage()
+    email['from'] = email_address
+    email['to'] = receiver
+    email['subject'] = subject
     email.set_content(message)
     server.send_message(email)
 
-def email(send_mail_to,send_subject,send_message):
+def email(send_mail_to, send_subject, send_message):
     name = send_mail_to
     print(f"{RED}{bot_name}{RESET}:Mail Details:-")
     print(f"{RED}{bot_name}{RESET}:Receiver name:", name)
 
-    if name in dict:
-        receiver = dict[name]
-        # subject = listen()
+    with open('data_archive/emails.json') as f:
+        emails_dict = json.load(f)
+
+    if name in emails_dict:
+        receiver = emails_dict[name]
         subject = send_subject
         print(f"{RED}{bot_name}{RESET}:Mail subject:", subject)
-
-        # message = listen()
         message = send_message
         print(f"{RED}{bot_name}{RESET}:Mail message:", message)
         send_mail(receiver, subject, message)
@@ -51,5 +49,3 @@ def email(send_mail_to,send_subject,send_message):
         # Returning None as a placeholder for error handling
         return None, None, None
 
-
-# main_function_code()  # calling the main function
